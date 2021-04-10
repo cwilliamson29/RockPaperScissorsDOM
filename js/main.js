db7bb222fa86725dfa74c799530b6a96f9d52928
Chris Williamson <chriswill0629@gmail.com>
2021-04-10 03:21:49 -0400

    javascript bug fixing

--- js/main.js
+++ js/main.js
@@ -4,10 +4,13 @@ let roundsBox = document.getElementById("numberRounds")
 
 let welcome = "Choose Your WEAPON!";
 let lost = "YOU LOST THIS ROUND!";
-let won = "YOU WON THIS ROUND!"
-let wonGame = "YOU WON\nTHE GAME!"
-let wonGame1 = "YOU WON"
-let wonGame2 = "THE GAME!"
+let won = "YOU WON THIS ROUND!";
+let wonGame = "YOU WON THE GAME!";
+
+let startWidth = "190px";
+let wonWidth = "155px";
+
+let timer;
 let speed = 110;
 
 userScore = document.querySelector("#playerWin p");
@@ -18,62 +21,33 @@ computerScore.textContent = computerWin;
 
 roundsBox.value = 5;
 
-let b = 0;
-function welcomeWriter(){
-    document.getElementById("welcome").innerHTML = "";
-    let b = 0;
 
-    function welRepeat(){
-        if(b < welcome.length){
-            document.getElementById("welcome").innerHTML += welcome.charAt(b);
-            b++;
-            setTimeout(welRepeat, speed);
-        }
-    }
-    welRepeat();
+//PASS VARIABLE'S WONGAME, LOST, WON, AND WELCOME
+//INTO CLEARWRITER(ACTION, WIDTH) TO PRODUCE THE ANIMATION
+//WIDTH IS DECLARED WITH THE STARTWIDTH AND THE WONWIDTH VARIABLES
+let cancel = false;
+let w = 0;
+function clearWriter(gameStatus, width){
+    clearTimeout(timer);
+    document.getElementById("welcome").innerHTML = "";
+    document.getElementById("welcome").style.width = width;
+    w = 0;
+    return animation(gameStatus);
 }
-function lostWriter(){
-    document.getElementById("welcome").innerHTML = "";
-    let w = 0;
 
-    function lostRepeat(){
-        if(w < lost.length){
-            document.getElementById("welcome").innerHTML += lost.charAt(w);
+function animation(gameStatus){
+    let lineRepeat = gameStatus;
+    function repeater(){
+        if(w < lineRepeat.length){
+            document.getElementById("welcome").innerHTML += lineRepeat.charAt(w);
             w++;
-            setTimeout(lostRepeat, speed);
+            timer = setTimeout(repeater, speed);
         }
     }
-lostRepeat();
-}
-function wonWriter(){
-    document.getElementById("welcome").innerHTML = "";
-    let w = 0;
-
-    function wonRepeat(){
-        if(w < won.length){
-            document.getElementById("welcome").innerHTML += won.charAt(w);
-            w++;
-            setTimeout(wonRepeat, speed);
-        }
-    }
-wonRepeat();
-}
-function wonGameWriter(){
-    document.getElementById("welcome").innerHTML = "";
-    document.getElementById("welcome").style.width = "155px";
-
-    let w = 0;
-
-    function wonRepeat(){
-        if(w < wonGame.length){
-            document.getElementById("welcome").innerHTML += wonGame.charAt(w);
-            w++;
-            setTimeout(wonRepeat, speed);
-        }
-    }
-wonRepeat();
+    //cancel = false;
+    return repeater()
 }
 
-welcomeWriter();
-//lostWriter();
+clearWriter(welcome, startWidth);
+
 
