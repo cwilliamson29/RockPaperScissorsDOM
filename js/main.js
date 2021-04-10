7aca4cc2fa5ff4a69ac70f1f33996fdc35f3a4ad
Chris Williamson <chriswill0629@gmail.com>
2021-04-10 14:30:17 -0400

    adding javascript logic

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ let roundsBox = document.getElementById("numberRounds")
 let welcome = "Choose Your WEAPON!";
 let lost = "YOU LOST THIS ROUND!";
 let won = "YOU WON THIS ROUND!";
+let tie = "YOU TIED THIS ROUND!"
 let wonGame = "YOU WON THE GAME!";
 
 let startWidth = "190px";
@@ -16,6 +17,7 @@ let speed = 110;
 let computerNum;
 let computerChoice;
 
+//SCORE BOARD AND ROUNDS
 userScore = document.querySelector("#playerWin p");
 userScore.textContent = playerWin;
 
@@ -37,22 +39,19 @@ let playerRockAlert = document.getElementById("playerRock");
 let playerPaperAlert = document.getElementById("playerPaper");
 let playerScissorsAlert = document.getElementById("playerScissors");
 
+//MOUSE OVER FOR ROCK SELECTION
 playerRockAlert.addEventListener("mouseover", function (){document.querySelector("#choiceAlert").innerHTML = rockAlert;});
 playerRockAlert.addEventListener("mouseout", function (){document.querySelector("#choiceAlert").innerHTML = choiceClearInner;});
 
+//MOUSE OVER FOR PAPER SELECTION
 playerPaperAlert.addEventListener("mouseover", function (){document.querySelector("#choiceAlert").innerHTML = paperAlert;});
 playerPaperAlert.addEventListener("mouseout", function (){document.querySelector("#choiceAlert").innerHTML = choiceClearInner;});
 
+//MOUSE OVER FOR SCISSORS SELECTION
 playerScissorsAlert.addEventListener("mouseover", function (){document.querySelector("#choiceAlert").innerHTML = scissorsAlert;});
 playerScissorsAlert.addEventListener("mouseout", function (){document.querySelector("#choiceAlert").innerHTML = choiceClearInner;});
 
-/*function hoverAlert(choice,choiceAlert){
-    let choice2 = choice;
-    console.log(choice2);
-    let choiceAlert2 = choiceAlert;
-    console.log(choiceAlert2);
-    document.getElementById(choice2).innerHTML = choiceAlert2;
-}*/
+
 //PASS VARIABLE'S WONGAME, LOST, WON, AND WELCOME
 //INTO CLEARWRITER(ACTION, WIDTH) TO PRODUCE THE ANIMATION
 //WIDTH IS DECLARED WITH THE STARTWIDTH AND THE WONWIDTH VARIABLES
@@ -80,26 +79,90 @@ function animation(gameStatus){
 /**************************
  ****Computer Selection****
  *************************/
+
+ //      Rock = 0
+//     Paper = 1
+// Scissor's = 2
+
 function computerSelection(){
     computerNum = Math.floor(Math.random(3) * 3);
 
     if(computerNum === 0){
-        computerChoice = "Rock";
+        computerChoice = 0;
+        return computerChoice;
 
-        console.log("The computer selected " + computerChoice);
     }else if(computerNum === 1){
-        computerChoice = "Paper";
+        computerChoice = 1;
+        return computerChoice;
 
-        console.log("The computer selected " + computerChoice);
     }else if(computerNum === 2){
-        computerChoice = "Scissors";
+        computerChoice = 2;
+        return computerChoice;
+    }
+}
 
-        console.log("The computer selected " + computerChoice);
-    }else{
-        console.log("Number error generated outside 0-2")
+/**************************
+ *****PLAYER Selection*****
+ *************************/
+
+ //      Rock = 0
+//     Paper = 1
+// Scissor's = 2
+let rock = 0;
+let paper = 1;
+let scissors = 2;
+
+//PLAYER SELECTION
+
+// ROCK SELECTOR
+document.getElementById("playerRock").addEventListener("click", computerSelection);
+document.getElementById("playerRock").addEventListener("click", function (){clearWriter(lost, startWidth)});
+document.getElementById("playerRock").addEventListener("click", function (){roundWinner(rock)});
+
+//PAPER SELECTOR
+document.getElementById("playerPaper").addEventListener("click", computerSelection);
+document.getElementById("playerPaper").addEventListener("click", function (){clearWriter(lost, startWidth)});
+document.getElementById("playerPaper").addEventListener("click", function (){roundWinner(rock)});
+
+//SCISSORS SELECTOR
+document.getElementById("playerScissors").addEventListener("click", computerSelection);
+document.getElementById("playerScissors").addEventListener("click", function (){clearWriter(lost, startWidth)});
+document.getElementById("playerScissors").addEventListener("click", function (){roundWinner(rock)});
+
+
+//DETERMINE ROUND WINNER
+function roundWinner(playerChoice){
+    if(playerChoice === computerChoice){
+        return clearWriter(tie, startWidth);
+    }else if(playerChoice === 0 && computerChoice === 1){   //rock loses to paper
+        //computerWin = ++computerWin;
+        computerScore.textContent = ++computerWin;
+        return clearWriter(lost, startWidth);
+
+    }else if(playerChoice === 0 && computerChoice === 2){   //rock beats scissors
+        userScore.textContent = ++playerWin;
+        return clearWriter(won, startWidth);
+
+    }else if(playerChoice === 1 && computerChoice === 0){   //paper beats rock
+        //playerWin = ++playerWin;
+        userScore.textContent = ++playerWin;
+        return clearWriter(won, startWidth);
+
+    }else if(playerChoice === 1 && computerChoice === 2){   //paper looses to scissors
+        //computerWin = ++computerWin;
+        computerScore.textContent = ++computerWin;
+        return clearWriter(lost, startWidth);
+
+    }else if(playerChoice === 2 && computerChoice === 0){   //scissors looses to rock
+        //computerWin = ++computerWin;
+        computerScore.textContent = ++computerWin;
+        return clearWriter(lost, startWidth);
+
+    }else if(playerChoice === 2 && computerChoice === 1){   //scissors beats paper
+        //playerWin = ++playerWin;
+        userScore.textContent = ++playerWin;
+        return clearWriter(won, startWidth);
     }
 }
 
 clearWriter(welcome, startWidth);
-
-
