611802f4c7ef5ad3dbc92343996bdda3f143f945
Chris Williamson <chriswill0629@gmail.com>
2021-04-10 16:36:36 -0400

    fixing round selector bugs, work in progress

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 let playerWin = 0;
 let computerWin = 0;
-let roundsBox = document.getElementById("numberRounds")
+let roundTie;
+let roundsBox = Number(document.getElementById("numberRounds").value);
 
 let welcome = "Choose Your WEAPON!";
 let lost = "YOU LOST THIS ROUND!";
 let won = "YOU WON THIS ROUND!";
 let tie = "YOU TIED THIS ROUND!"
 let wonGame = "YOU WON THE GAME!";
+let lostGame = "YOU LOST THE GAME!";
 
 let startWidth = "190px";
 let wonWidth = "155px";
@@ -24,7 +26,7 @@ userScore.textContent = playerWin;
 computerScore = document.querySelector("#computerWin p");
 computerScore.textContent = computerWin;
 
-roundsBox.value = 5;
+//roundsBox.value = 5;
 
 /*********************
  ****HOVER EFFECTS****
@@ -120,53 +122,112 @@ let scissors = 2;
 
 // ROCK SELECTOR
 document.getElementById("playerRock").addEventListener("click", computerSelection);
-document.getElementById("playerRock").addEventListener("click", function (){clearWriter(lost, startWidth)});
 document.getElementById("playerRock").addEventListener("click", function (){roundWinner(rock)});
 
 //PAPER SELECTOR
 document.getElementById("playerPaper").addEventListener("click", computerSelection);
-document.getElementById("playerPaper").addEventListener("click", function (){clearWriter(lost, startWidth)});
 document.getElementById("playerPaper").addEventListener("click", function (){roundWinner(paper)});
 
 //SCISSORS SELECTOR
 document.getElementById("playerScissors").addEventListener("click", computerSelection);
-document.getElementById("playerScissors").addEventListener("click", function (){clearWriter(lost, startWidth)});
 document.getElementById("playerScissors").addEventListener("click", function (){roundWinner(scissors)});
 
 
 //DETERMINE ROUND WINNER
 function roundWinner(playerChoice){
     if(playerChoice === computerChoice){
+        roundTie = true;
         return clearWriter(tie, startWidth);
     }else if(playerChoice === 0 && computerChoice === 1){   //rock loses to paper
-        //computerWin = ++computerWin;
+        roundTie = false;
         computerScore.textContent = ++computerWin;
-        return clearWriter(lost, startWidth);
+        return clearWriter(lost, startWidth), roundsSelector();
 
     }else if(playerChoice === 0 && computerChoice === 2){   //rock beats scissors
+        roundTie = false;
         userScore.textContent = ++playerWin;
-        return clearWriter(won, startWidth);
+        return clearWriter(won, startWidth), roundsSelector();
 
     }else if(playerChoice === 1 && computerChoice === 0){   //paper beats rock
-        //playerWin = ++playerWin;
+        roundTie = false;
         userScore.textContent = ++playerWin;
-        return clearWriter(won, startWidth);
+        return clearWriter(won, startWidth), roundsSelector();
 
     }else if(playerChoice === 1 && computerChoice === 2){   //paper looses to scissors
-        //computerWin = ++computerWin;
+        roundTie = false;
         computerScore.textContent = ++computerWin;
-        return clearWriter(lost, startWidth);
+        return clearWriter(lost, startWidth), roundsSelector();
 
     }else if(playerChoice === 2 && computerChoice === 0){   //scissors looses to rock
-        //computerWin = ++computerWin;
+        roundTie = false;
         computerScore.textContent = ++computerWin;
-        return clearWriter(lost, startWidth);
+        return clearWriter(lost, startWidth), roundsSelector();
 
     }else if(playerChoice === 2 && computerChoice === 1){   //scissors beats paper
-        //playerWin = ++playerWin;
+        roundTie = false;
         userScore.textContent = ++playerWin;
-        return clearWriter(won, startWidth);
+        return clearWriter(won, startWidth), roundsSelector();
     }
 }
 
-clearWriter(welcome, startWidth);
+/**************************
+ *****ROUNDS Selection*****
+ *************************/
+
+function roundsSelector(){
+    //roundsBox = document.getElementById("numberRounds").value;
+    roundsCount = roundsBox;
+    if(roundsCount == 0){
+        return gameWinner()
+    }else{
+         --roundsCount;
+         --roundsBox;
+         document.getElementById("numberRounds").value = roundsBox;
+         //roundsBox = document.getElementById("numberRounds").value;
+         //roundsBox
+    }
+}
+
+ /**************************
+ ********GAME WINNER********
+ **************************/
+
+ function gameWinner(){
+     if(playerWin > computerWin){
+        document.getElementById("computerChoiceIMG").src = "img/blank.png";
+
+        document.getElementById("playerRock").addEventListener("click", clearGame);
+
+        //PAPER SELECTOR
+        document.getElementById("playerPaper").addEventListener("click", clearGame);
+
+        //SCISSORS SELECTOR
+        document.getElementById("playerScissors").addEventListener("click", location.reload());
+        return clearWriter(wonGame, wonWidth)
+     }else if(playerWin < computerWin){
+        document.getElementById("computerChoiceIMG").src = "img/blank.png";
+        return clearWriter(lostGame, wonWidth)
+     }
+ }
+
+/**************************
+ ********CLEAR GAME********
+ **************************/
+function clearGame(){
+    document.getElementById("numberRounds").value = 5;
+    playerWin = 0;
+    computerWin = 0;
+
+    //document.getElementById("playerRock").addEventListener("click", computerSelection);
+    //document.getElementById("playerRock").addEventListener("click", function (){roundWinner(rock)});
+
+    //PAPER SELECTOR
+    //document.getElementById("playerPaper").addEventListener("click", computerSelection);
+    //document.getElementById("playerPaper").addEventListener("click", function (){roundWinner(paper)});
+
+    //SCISSORS SELECTOR
+    //document.getElementById("playerScissors").addEventListener("click", computerSelection);
+    //document.getElementById("playerScissors").addEventListener("click", function (){roundWinner(scissors)});
+}
+
+//clearWriter(welcome, startWidth);
